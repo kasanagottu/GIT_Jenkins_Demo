87dbc1791980d670e2e498dbfe0e1a50f7dcc33b
kasanagottu <venkatesh.kasanagottu@gmail.com>
2021-02-11 11:42:21 +0530

    This is first Jenkins and Git Script

---
+++ FirstDemo_GIT_Genkins/src/test/java/Pkg/git_jenkins/GIT_Jenkins_DemoTest.java
@@ -0,0 +1,25 @@
+package Pkg.git_jenkins;
+
+import org.testng.annotations.Test;
+
+public class GIT_Jenkins_DemoTest {
+	
+	@Test
+	public void firsttest()
+	{
+		System.out.println("This is first method");
+	}
+	@Test
+	public void secondtest()
+	{
+		System.out.println("This is second method");
+	}
+	@Test
+	public void thirdtest()
+	{
+		System.out.println("This is third method");
+	}
+
+
+
+}
